5c088473a2435fe4d1c26e216454a7db40a66562
Diego Varzim <diego.varzim@hotmail.com>
2023-06-07 10:24:31 -0300

    ok

--- janela.py
+++ janela.py
@@ -16,6 +16,9 @@
 # janela.mainloop()
 import customtkinter
 
+customtkinter.set_appearance_mode('dark')
+customtkinter.set_default_color_theme('dark-blue')
+
 janela = customtkinter.CTk()
 janela.geometry('500x300')
 
@@ -30,6 +33,9 @@ email.pack(padx=10, pady=10)
 
 senha = customtkinter.CTkEntry(janela, placeholder_text='Digita a sua senha', show='*')
 senha.pack(padx=10, pady=10)
+
+checkbox = customtkinter.CTkCheckBox(janela, text='Lembrar Login')
+checkbox.pack(padx=10, pady=10)
 
 botao = customtkinter.CTkButton(janela, text='Login', command=clique)
 botao.pack(padx=10, pady=10)
